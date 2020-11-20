54e596f144bf6e9f77e8919fc50cd82c21c19f3b
ariful-islam-mahin <mahinyayaa@gmail.com>
2020-11-20 21:55:12 +0600

    complete

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12 col-md-6 banner-text">
-                        <p className="banner-text-header">Florence agency</p>
+                        <h1 className="banner-text-header">Florence agency</h1>
                         <p className="banner-text-small"><small>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</small>
                         </p>
                         <button className="banner-btn">See Pricing</button>
