2438bbd02918c6ae57b1f243a6265ad21d3bc6d4
ariful-islam-mahin <mahinyayaa@gmail.com>
2020-11-20 21:32:59 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Team from "./components/Team/Team";
 import Services from "./components/Services/Services";
 import StayProject from "./components/StayProject/StayProject";
 import Achievements from "./components/Achievements/Achievements";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="main">
+      <Banner/>
       <Services />
       <StayProject />
       <Achievements/>
