5a4d5db18fd26b4ef87922e74486d9a132cb3693
shawonahmed1 <mdshawon356@gmail.com>
2020-11-20 12:10:19 +0600

    after add whatwe and stayproject section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Contact from './components/Contact/Contact';
-import Team from './components/Team/Team';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contact from "./components/Contact/Contact";
+import Team from "./components/Team/Team";
+import What from "./components/What/What";
+import StayProject from "./components/StayProject/StayProject";
 
 function App() {
   return (
     <div className="main">
-      <Team/>
-      <Contact/>
-      <Footer/> 
+      <Team />
+      <StayProject />
+      <What />
+      <Contact />
+      <Footer />
     </div>
   );
 }
