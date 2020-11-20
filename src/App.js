6fe30caa82ec7e54854889749b8b584876dce9b4
ariful-islam-mahin <mahinyayaa@gmail.com>
2020-11-20 10:15:23 +0600

    team section added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './components/Contact/Contact';
+import Team from './components/Team/Team';
 
 function App() {
   return (
     <div className="main">
+      <Team/>
       <Contact/>
       <Footer/> 
     </div>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <section className="text-center contact container">
-            <h1 className="font-weight-bold">Get your design right, right now</h1>
+        <section className="text-center contact container mt-5">
+            <h1 className="header">Get your design right, right now</h1>
             <p>Be the first know our latest offers and updates!</p>
-            <div class="input-group mb-3 p-2 rounded bg-white my-5 contact-input mx-auto">
+            <div class="d-flex align-items-center input-group mb-3 p-2 rounded bg-white my-5 contact-input mx-auto">
                 <input type="text" class="form-control border-0" placeholder="Enter your email address"/>
                 <div class="input-group-append">
-                    <button class="btn btn-primary rounded px-4" type="button">Get Started</button>
+                    <button class="btn gradient-btn rounded px-4" type="button">Get Started</button>
                 </div>
             </div>
         </section>
